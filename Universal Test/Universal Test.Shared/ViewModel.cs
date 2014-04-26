6ed914b48bb222fa033cb2ac255d1c2410da448c
Arve Knudsen <arve.knudsen@bouvet.no>
2014-04-26 23:53:19 +0200

    Revise UI

--- Universal Test/Universal Test.Shared/ViewModel.cs
+++ Universal Test/Universal Test.Shared/ViewModel.cs
@@ -6,16 +6,18 @@ namespace Universal_Test
 {
     class ItemModel
     {
-        public ItemModel(string name, string date, string description)
+        public ItemModel(string name, string date, string description, Uri itemUri)
         {
             Name = name;
             Date = date;
             Description = description;
+            ItemUri = itemUri;
         }
 
         public string Name { get; set; }
         public string Date {  get; set; }
         public string Description { get; set; }
+        public Uri ItemUri { get; set; }
     }
 
     class ViewModel
@@ -28,8 +30,8 @@ namespace Universal_Test
             {
                 return _itemModels ?? (_itemModels = new List<ItemModel>
                 {
-                    new ItemModel("Item 1", "25.04.2014", "Description 1"),
-                    new ItemModel("Item 2", "25.04.2014", "Description 2")
+                    new ItemModel("Item 1", "25.04.2014", "Description 1", new Uri("http://example.com")),
+                    new ItemModel("Item 2", "25.04.2014", "Description 2", new Uri("http://example.com"))
                 });
             }
             set
